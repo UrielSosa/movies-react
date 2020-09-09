dd42df5754034a980da544f6f71f00a5038bbdac
Uriel Sosa <sosa.uriel1999@gmail.com>
2020-09-09 18:59:50 -0300

    button in header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,9 @@ function Header () {
                 <form className="form-inline w-50 d-flex justify-content-center">
                     <input className="form-control w-75" type="search" placeholder="Buscá una peli..." aria-label="Search" />
                 </form>
-                <button className="btn btn-primary" type="button">Crear pelicula</button>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#crearMovie">
+                    Crear Pelicula
+                </button>
             </nav>
         </header>
     )
